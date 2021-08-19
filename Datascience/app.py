0930f6340672d4de33c005a7230a0824a0a7e7f4
Sanjay-code-creator <67993505+Sanjay-code-creator@users.noreply.github.com>
2021-08-19 10:04:45 +0530

    Add files via upload

---
+++ Datascience/app.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,render_template
+import pandas as pd
+import numpy as np
+import pickle
+from flasgger import Swagger
+app=Flask(__name__)
+Swagger(app)
+pickle_in=open('classifier.pkl','rb')
+classifier=pickle.load(pickle_in)
+@app.route('/')
+def main():
+    return "Wellcome"
+@app.route('/predict',methods=['GET'])
+def check():
+    
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    return "The predicted value is " + str(prediction)
+if __name__=="__main__":
+    app.run()
